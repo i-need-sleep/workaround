7604fdb4f2fbbb216bb9e1d3d378680dbb295b67
i-need-sleep <yh2689@nyu.edu>
2025-02-05 20:07:50 -0500

    mad demo

--- debug.py
+++ debug.py
@@ -1,5 +1,11 @@
 import json
 
 # write a random json
-with open('debug.json', 'w') as f:
-    json.dump({'a': 1, 'b': 2}, f)
+# with open('debug.json', 'w') as f:
+#     json.dump({'a': 1, 'b': 2}, f)
+
+# read the json
+with open('./mad_demo/src/assets/demo_metadata.json', 'r') as f:
+    data = json.load(f)
+    print(data[9].keys())
+    
